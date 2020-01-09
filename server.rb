0dcbe78bebaadec312a533ca5613919a65d13f11
unknown <thyme400@gmail.com>
2020-01-09 13:20:15 -0500

    make it better

--- server.rb
+++ server.rb
@@ -42,6 +42,8 @@ end
 post '/login' do
   user = User.find_by(email: params[:email])
   given_password = params[:password]
+if @user.valid?
+  # @user.save
   if user.password == given_password
     session[:user_id] = user.id
     redirect '/profile'
@@ -49,6 +51,9 @@ post '/login' do
     flash[:error] = "Invalid Email or Password"
     redirect "/login"
   end
+
+end
+
 end
 
 
